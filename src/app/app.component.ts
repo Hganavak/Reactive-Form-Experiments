277de91aaf402d6413dbefdb391021a4c2005d50
Hganavak <hganavak@gmail.com>
2019-05-10 14:43:03 +1200

    Created custom form validator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup, FormBuilder, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
@@ -7,9 +7,42 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  myFormControl: FormControl;
+  myFormControl: FormControl; // Used in state examples
 
-  constructor() {
+  myFormGroup: FormGroup;
+
+  constructor(formBuilder: FormBuilder) {
+    // Email/Pass Example (FormBuilder Approach)
+    this.myFormGroup = formBuilder.group(
+      {
+        email: ["", [Validators.required, Validators.email]],
+        password: ["", [Validators.required, Validators.minLength(8)]],
+        confirm_password: ["", [Validators.required, Validators.minLength(8)]],
+      },
+      {
+        validators: [passwordMatch] // Custom validators
+      }
+    );
+
+
+    // State Example (Imperitive Approach)
     this.myFormControl = new FormControl('Default Value', [Validators.required, Validators.minLength(9)]); // The initial value of the form element
   }
+}
+
+// Custom Validator (Simply a function that returns ValidationErrors)
+// These can be created either at the group or the control level
+function passwordMatch(formGroup: FormGroup) : ValidationErrors | undefined {
+  const  passwordControl = formGroup.get('password');
+  const confirmPasswordControl = formGroup.get('confirm_password');
+
+  if(passwordControl.value === confirmPasswordControl.value) {
+    return null;
+  } else {
+    return {
+      passwordMatch: true // <--- This is the name of the ERROR
+    }
+  }
+
+  return undefined;
 }
